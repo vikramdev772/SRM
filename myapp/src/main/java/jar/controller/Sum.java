c59efb3132aed3ad68fd798248be9eec3255bc7f
aditya23409876543 <dhanrajaditya230@gmail.com>
2026-05-22 09:56:42 +0530

    sum.java sample

--- myapp/src/main/java/jar/controller/Sum.java
+++ myapp/src/main/java/jar/controller/Sum.java
@@ -1,5 +1,14 @@
 package jar.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Sum {
     
+    @GetMapping("/api/sum")
+    public int sum(@RequestParam int a, @RequestParam int b) {
+        return a + b;
+    }
 }
