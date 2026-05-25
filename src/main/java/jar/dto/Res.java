4bd75df6ff617463892337eebc33c9db06a99f52
vikramdev772 <vikramdev772@gmail.com>
2026-05-25 08:22:53 +0530

    fix 🔧

--- src/main/java/jar/dto/Res.java
+++
@@ -1,8 +0,0 @@
-package jar.dto;
-
-public class Res {
-
-    public Object name = "spring boot";
-    public Object msg = "server started sucess";
-    public Object status = 200;
-}
--- src/main/java/jar/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package jar.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import jar.model.User;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-
-}
